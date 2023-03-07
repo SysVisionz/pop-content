b9a0cea7e843a1acf0c5bb2e82c7ce73af1f0d0e
SysVisionz <colin@sysvisionz.com>
2023-03-07 09:58:26 -0800

    style loader fix may allow ssr

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     devtool: "source-map",
     module: {
       rules: [
-        { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ] },
+        { test: /\.scss$/, use: [ "isomorphic-style-loader", "css-loader", "sass-loader" ] },
         { test: /\.tsx?$/, loader: "babel-loader" },
         { test: /\.tsx?$/, loader: "ts-loader" },
         { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
